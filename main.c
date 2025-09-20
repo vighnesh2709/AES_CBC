9b7f19e95f60c77763c0f12f5c525f1fd44408d8
vighnesh2709 <bvighnesh27@gmail.com>
2025-09-20 15:08:54 +0530

    CBC done

--- main.c
+++ main.c
@@ -1,118 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "encryption.h"
 #include "decryption.h"
 #include "helpers.h"
 #include "keyScheduling.h"
 
-// // AES-128 encryption (10 rounds)
-// void encryptBlock(unsigned char block[16]) {
-//     // Initial round key
-//     addRoundKey(block, roundKeys[0]);
-
-//     // 9 main rounds
-//     for (int round = 1; round < 10; round++) {
-//         subBytes(block);
-//         shiftRows(block);
-//         mixColumns(block);
-//         addRoundKey(block, roundKeys[round]);
-//     }
-
-//     // Final round (no mixColumns)
-//     subBytes(block);
-//     shiftRows(block);
-//     addRoundKey(block, roundKeys[10]);
-// }
-
-// // AES-128 decryption (10 rounds)
-// void decryptBlock(unsigned char block[16]) {
-//     // Initial round (use last round key)
-//     addRoundKey(block, roundKeys[10]);
-
-//     // 9 main rounds (inverse order)
-//     for (int round = 9; round > 0; round--) {
-//         invShiftRows(block);
-//         invSubBytes(block);
-//         addRoundKey(block, roundKeys[round]);
-//         invMixColumns(block);
-//     }
-
-//     // Final round
-//     invShiftRows(block);
-//     invSubBytes(block);
-//     addRoundKey(block, roundKeys[0]);
-// }
-
-
-
 int main()
 {
+    
+    FILE *fp = fopen("test_data/sample.txt", "rb");
+    FILE *encOut = fopen("test_data/encrypted.bin", "wb");
+    if (!fp || !encOut)
+    {
+        printf("Error while opening input/encrypt files\n");
+        return 1;
+    }
 
     unsigned char block[16];
     unsigned char key[16];
+    unsigned char IV[16];
+    unsigned char prevCipher[16];
+    size_t bytesRead;
 
-    printf("Enter 16 unsigned characters (AES Block)");
+    
+    printf("Enter 16 unsigned characters (AES Key): ");
     for (int i = 0; i < 16; i++)
     {
-
-        int index = (i % 4) * 4 + (i / 4);
-        scanf(" %c", &block[index]);
-    }
-
-    printf("Block:");
-    printBlock(block);
-
-    printf("Enter 16 unsigned character key (AES Block)");
-    for (int i = 0; i < 16; i++)
-    {
-
         int index = (i % 4) * 4 + (i / 4);
         scanf(" %c", &key[index]);
     }
 
-    keyGen(key,0);
+    
+    printf("Enter 16 unsigned characters (IV): ");
+    for (int i = 0; i < 16; i++)
+    {
+        int index = (i % 4) * 4 + (i / 4);
+        scanf(" %c", &IV[index]);
+    }
 
-    // for(int i = 0 ; i < 11; i++){
-    //     printHex(roundKeys[i]);
-    // }
+    
+    keyGen(key, 0);
 
-    /*
-    initalround key
-    10 rounds
-    encrypted data
+   
+    memcpy(prevCipher, IV, 16);
 
-    use last key from table as main round key
-    and do decryption
-    */
+    
+    while ((bytesRead = fread(block, 1, 16, fp)) > 0)
+    {
+        
+        if (bytesRead < 16)
+        {
+            unsigned char padVal = 16 - bytesRead;
+            for (size_t i = bytesRead; i < 16; i++)
+            {
+                block[i] = padVal;
+            }
+        }
 
-    encryptBlock(block);
-    printf("\nEncrypted Block: ");
-    printHex(block);
+        
+        for (int i = 0; i < 16; i++)
+        {
+            block[i] ^= prevCipher[i];
+        }
 
-    // Decrypt
-    decryptBlock(block);
-    printf("\nDecrypted Block: ");
-    printBlock(block);
+        
+        encryptBlock(block);
+
+        
+        fwrite(block, 1, 16, encOut);
+
+        
+        memcpy(prevCipher, block, 16);
+    }
+
+    
+    if (bytesRead == 0 && ftell(fp) % 16 == 0)
+    {
+        unsigned char padBlock[16];
+        unsigned char padVal = 16;
+        for (int i = 0; i < 16; i++)
+        {
+            padBlock[i] = padVal ^ prevCipher[i];
+        }
+        encryptBlock(padBlock);
+        fwrite(padBlock, 1, 16, encOut);
+    }
+
+    fclose(fp);
+    fclose(encOut);
+
+    printf("Encryption complete. Output written to encrypted.bin\n");
+
+    
+    FILE *encIn = fopen("test_data/encrypted.bin", "rb");
+    FILE *decOut = fopen("test_data/decrypted.txt", "wb");
+    if (!encIn || !decOut)
+    {
+        printf("Error while opening decrypt files\n");
+        return 1;
+    }
+
+    unsigned char curCipher[16];
+    memcpy(prevCipher, IV, 16);
+
+    
+    fseek(encIn, 0, SEEK_END);
+    long fileSize = ftell(encIn);
+    rewind(encIn);
+
+    size_t totalBytes = 0;
+
+    while ((bytesRead = fread(curCipher, 1, 16, encIn)) > 0)
+    {
+        memcpy(block, curCipher, 16);
+        decryptBlock(block);         
+
+        for (int i = 0; i < 16; i++)
+        {
+            block[i] ^= prevCipher[i];
+        }
+
+        
+        memcpy(prevCipher, curCipher, 16);
+        totalBytes += bytesRead;
+
+        
+        if (totalBytes == fileSize)
+        {
+            unsigned char padVal = block[15];
+            if (padVal > 0 && padVal <= 16)
+            {
+                bytesRead -= padVal;
+            }
+        }
+
+        fwrite(block, 1, bytesRead, decOut);
+    }
+
+    fclose(encIn);
+    fclose(decOut);
+
+    printf("Decryption complete. Output written to decrypted.txt\n");
 
     return 0;
 }
-
-/*
-AES
-
-Main Fn
--------
-Sub Bytes
-Shift Rows
-Mic Column
-Add Round Key
-
-
-Key Gen
--------
-4 Words
-Shift wors
-XOR
-Rij part
-
-*/
